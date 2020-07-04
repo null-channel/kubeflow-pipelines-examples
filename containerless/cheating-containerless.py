8c7a3f6257c60288813085e6235bc0d1fc5c5a32
Marek Counts <countsmarek@gmail.com>
2020-07-04 01:44:32 -0400

    silly updates

--- containerless/cheating-containerless.py
+++ containerless/cheating-containerless.py
@@ -5,17 +5,17 @@ from kfp import dsl
 @dsl.pipeline(name='my-pipeline')
 def pipeline():
     op0 = dsl.ContainerOp(
-        name="my-out-cop0",
+        name="gen-numbers",
         image='python:alpine3.6',
         command=["sh", "-c"],
         arguments=[
-            'python -c "import random; import json; import sys; json.dump([i for i in range(20, 31)], open(\'/tmp/out.json\', \'w\'))"'],
+            'python -c "import random; import json; import sys; json.dump([i for i in range(20, 26)], open(\'/tmp/out.json\', \'w\'))"'],
         file_outputs={'out': '/tmp/out.json'},
     )
 
     with dsl.ParallelFor(op0.output) as item:
         op1 = dsl.ContainerOp(
-            name="my-in-cop1",
+            name="my-item-print",
             image="library/bash:4.4.23",
             command=["sh", "-c"],
             arguments=["echo do output op1 item: %s" % item],
@@ -25,7 +25,7 @@ def pipeline():
         name="total",
         image="python:alpine3.6",
         command=["sh", "-c"],
-        arguments=['echo do output op2, outp: %s && python -c "print(sum(%s))"' % (op0.output, op0.output)],
+        arguments=['echo output gen-numbers: %s && python -c "print(sum(%s))"' % (op0.output, op0.output)],
     )
 
 
